4a9b057967e85187c409973287f0283a637b4760
Jose Juarez <josejuarez78@gmail.com>
2022-07-19 19:21:04 -0500

    Updating application version

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>José Juárez</p>
-        <p>Sample Application version 1.0</p>
+        <p>Sample Application version 1.1</p>
       </header>
     </div>
   );
